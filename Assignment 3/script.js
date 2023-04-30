45cb894e07935d191ece2cd551a2d4b89438d2ff
Alexander Mo <alexanderglmo@gmail.com>
2023-04-30 15:28:33 -0400

    Forgot to commit everything else earlier

--- Assignment 3/script.js
+++ Assignment 3/script.js
@@ -1,71 +1,45 @@
 import { key } from "./key.js";
 
 
-const getTMDBData = async (url) => {
-  return (await axios.get(url)).data;
-};
+document.getElementById("getbutton").addEventListener("click", getmovie);
 
-const createMovieTile = (id, poster, title, date, description) => {
-  const tile = document.createElement("div");
-  const details = document.createElement("div");
-  const img = document.createElement("img");
-  const h1 = document.createElement("h1");
-  const h3 = document.createElement("h3");
-  const h4 = document.createElement("h4");
-  const buyButton = document.createElement("button");
-  const trailerButton = document.createElement("button");
 
-  tile.classList.add("tile");
-  img.src = `https://image.tmdb.org/t/p/w500/${poster}`;
-  h1.innerText = title;
-  h3.innerText = date;
-  h4.innerText = description;
-  trailerButton.innerText = "Trailer";
+function getmovie() {
+  let movieId = parseInt(document.getElementById("list").value);
 
-  buyButton.addEventListener("click", () => {
-    cartContents.add(id);
-    const cart = document.getElementById("cart");
-    cart.innerHTML = `Your cart contains ${cartContents.size} movies`;
+  let search = axios.get("https://api.themoviedb.org/3/movie/" + movieId, {
+    params: {
+      api_key: key,
+      append_to_response: "videos",
+    },
   });
 
-  trailerButton.addEventListener("click", async () => {
-    const trailersData = await getTMDBData(
-      `https://api.themoviedb.org/3//movie/${id}/videos?api_key=${key}&language=en-US&adult=false`
-    );
+  let searchResult = search.then((movieData) => {
+    document.getElementById("poster").src =
+      "https://image.tmdb.org/t/p/w500" + movieData.data.poster_path;
 
-    const trailer = trailersData.results.filter((trailer) => {
-      return trailer.type === "Trailer";
-    });
-
-    !trailer.length
-      ? alert("Sorry! No trailers for this film.")
-      : window.open(`https://www.youtube.com/watch?v=${trailer.at(0).key}`);
+    document.getElementById("title").innerHTML = movieData.data.original_title;
+    document.getElementById("tagline").innerHTML = movieData.data.tagline;
+    document.getElementById(
+      "status"
+    ).innerHTML = `${movieData.data.status} ~ ${movieData.data.release_date}`;
+    document.getElementById(
+      "popularity"
+    ).innerHTML = `Popularity: ${movieData.data.popularity}`;
+    document.getElementById(
+      "voteAverage"
+    ).innerHTML = `Vote Average: ${movieData.data.vote_average}`;
+    document.getElementById(
+      "voteCount"
+    ).innerHTML = `Vote Count: ${movieData.data.vote_count}`;
+    document.getElementById(
+      "budget"
+    ).innerHTML = `Budget: $${movieData.data.budget}`;
+    document.getElementById("overview").innerHTML = movieData.data.overview;
+    document.getElementById("trailer").src =
+      "https://www.youtube.com/embed/" +
+      movieData.data.videos.results
+        .filter((trailer) => trailer.type === "Trailer")
+        .at(0).key;
   });
-
-  details.append(h1);
-  details.append(h3);
-  details.append(h4);
-
-  tile.append(img);
-  tile.append(details);
-  tile.append(buyButton);
-  tile.append(trailerButton);
-
-  return tile;
-};
-
-let movieData = await getTMDBData(
-  `https://api.themoviedb.org/3//movie/top_rated?api_key=${key}&language=en-US&adult=false`
-);
-const movies = document.getElementById("movies");
-
-movieData.results.forEach((movie) => {
-  const tile = createMovieTile(
-    movie.id,
-    movie.poster_path,
-    movie.title,
-    movie.release_date,
-    movie.overview
-  );
-  movies.appendChild(tile);
-});
+}
